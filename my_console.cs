c82823882bf0b387dc7da469e59d4b0f8a5fdc3e
thebearslicer <70990720+thebearslicer@users.noreply.github.com>
2021-06-10 14:12:12 -0400

    Fixed time command

--- my_console.cs
+++ my_console.cs
@@ -44,13 +44,23 @@ namespace CosmosOS
         }
         public static string CreateTimeString()
         {
+
+            string dayOfTheWeek;
+
+
             //byte byte_seconds = Cosmos.HAL.RTC.Second;
             //byte byte_minutes = Cosmos.HAL.RTC.Minute;
             //byte byte_hours   = Cosmos.HAL.RTC.Hour;
-            //byte byte_date    = Cosmos.HAL.RTC.DayOfTheMonth; //Cosmos.HAL.RTC.whatever returns a byte of the time/day or whatever you put into it
+            byte byte_date    = Cosmos.HAL.RTC.DayOfTheMonth; //Cosmos.HAL.RTC.whatever returns a byte of the time/day or whatever you put into it
             //byte byte_day     = Cosmos.HAL.RTC.DayOfTheWeek;
-            //byte byte_month   = Cosmos.HAL.RTC.Month;
-            //byte byte_year    = Cosmos.HAL.RTC.Year;
+            byte byte_month   = Cosmos.HAL.RTC.Month;
+            byte byte_year    = Cosmos.HAL.RTC.Year;
+
+            int int_date = (int)byte_date;
+            int int_month = (int)byte_month;
+            int int_year = (int)byte_year;
+
+            dayOfTheWeek = GetDayOfTheWeek(int_month, int_date, int_year);
 
             //string seconds    = byte_seconds.ToString();
             //string minutes    = byte_minutes.ToString();
@@ -60,10 +70,61 @@ namespace CosmosOS
             //string month      = byte_month.ToString();
             //string year       = byte_year.ToString();
             //put strings together to make something nice
-            string final_string = "It is " + Cosmos.HAL.RTC.DayOfTheWeek + ", " + Cosmos.HAL.RTC.Month + " " + Cosmos.HAL.RTC.DayOfTheMonth + " "  + Cosmos.HAL.RTC.Year + " at " + Cosmos.HAL.RTC.Hour + ":" + Cosmos.HAL.RTC.Minute + ":" + Cosmos.HAL.RTC.Second;
+            string final_string = "It is " + dayOfTheWeek + ", " + Cosmos.HAL.RTC.Month + "/" + Cosmos.HAL.RTC.DayOfTheMonth + "/" + Cosmos.HAL.RTC.Year + " at " + Cosmos.HAL.RTC.Hour + ":" + Cosmos.HAL.RTC.Minute + ":" + Cosmos.HAL.RTC.Second;
             return final_string;
-                                                                                                                               
+
+        }
+        public static string GetDayOfTheWeek(int month, int day, int year)
+        {
+
+            string day_string;
+
+            if (month < 3)
+            {
+                day += year;
+                year--;
+            }
+            else
+            {
+                day = day + year - 2;
+            }
+
+            int day_value = (23 * month / 9) + day + 4 + year / 4 - year / 100 + year / 400;
+            day_value = day_value % 7;
+
+            switch(day_value)
+            {
+                case 0:
+                    day_string = "Sunday";
+                    break;
+                case 1:
+                    day_string = "Monday";
+                    break;
+                case 2:
+                    day_string = "Tuesday";
+                    break;
+                case 3:
+                    day_string = "Wednesday";
+                    break;
+                case 4:
+                    day_string = "Thursday";
+                    break;
+                case 5:
+                    day_string = "Friday";
+                    break;
+                case 6:
+                    day_string = "Saturday";
+                    break; ;
+                default:
+                    day_string = "Error";
+                    break;
+            }
+            return day_string;
+             
+                
+
+           
         }
     }
-    
+
 }
